58d956b0440af2b7eadbedf6656417a01907ea3c
Unknown <railken@protonmail.ch>
2017-10-14 00:11:14 +0200

    add test paginator

---
+++ tests/PaginatorTest.php
@@ -0,0 +1,67 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Railken\ApiHelpers\Tests\Paginator;
+
+
+class PaginatorTest extends TestCase
+{
+
+
+    public function testLastPage()
+    {   
+        $paginator = new Paginator();
+        $pagination = $paginator->paginate(new \stdClass(), 100, 10);
+
+        $this->assertEquals(999, $pagination->total);
+        $this->assertEquals(10, $pagination->take);
+        $this->assertEquals(100, $pagination->page);
+        $this->assertEquals(100, $pagination->pages);
+        $this->assertEquals(990, $pagination->skip);
+        $this->assertEquals(991, $pagination->from);
+        $this->assertEquals(999, $pagination->to);
+
+    }
+
+    public function testFirstPage()
+    {   
+        $paginator = new Paginator();
+        $pagination = $paginator->paginate(new \stdClass(), 1, 10);
+
+        $this->assertEquals(999, $pagination->total);
+        $this->assertEquals(10, $pagination->take);
+        $this->assertEquals(1, $pagination->page);
+        $this->assertEquals(100, $pagination->pages);
+        $this->assertEquals(0, $pagination->skip);
+        $this->assertEquals(1, $pagination->from);
+        $this->assertEquals(10, $pagination->to);
+    }
+
+    public function testSecondPage()
+    {   
+        $paginator = new Paginator();
+        $pagination = $paginator->paginate(new \stdClass(), 2, 10);
+
+        $this->assertEquals(999, $pagination->total);
+        $this->assertEquals(10, $pagination->take);
+        $this->assertEquals(2, $pagination->page);
+        $this->assertEquals(100, $pagination->pages);
+        $this->assertEquals(10, $pagination->skip);
+        $this->assertEquals(11, $pagination->from);
+        $this->assertEquals(20, $pagination->to);
+    }
+
+    public function testMidPage()
+    {   
+        $paginator = new Paginator();
+        $pagination = $paginator->paginate(new \stdClass(), 51, 10);
+
+        $this->assertEquals(999, $pagination->total);
+        $this->assertEquals(10, $pagination->take);
+        $this->assertEquals(51, $pagination->page);
+        $this->assertEquals(100, $pagination->pages);
+        $this->assertEquals(500, $pagination->skip);
+        $this->assertEquals(501, $pagination->from);
+        $this->assertEquals(510, $pagination->to);
+    }
+}
